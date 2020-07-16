d56c86192a61c304beb53dbe21ea698a8d16f092
songwenyao <songwenyao@izaodao.org>
2020-07-16 14:38:47 +0800

    接入

--- main.php
+++ main.php
@@ -1,8 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: songwenyao
- * Date: 2020/7/16
- * Time: 11:02 AM
- */
-echo 1111;
+define('TOKEN', 'vooy123');
+
+if(checkSignature())
+    echo $_GET['echostr'];
+
+function checkSignature()
+{
+    $signature = $_GET["signature"];
+    $timestamp = $_GET["timestamp"];
+    $nonce = $_GET["nonce"];
+
+    $token = TOKEN;
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING);
+    $tmpStr = implode( $tmpArr );
+    $tmpStr = sha1( $tmpStr );
+
+    if( $tmpStr == $signature ){
+        return true;
+    }else{
+        return false;
+    }
+}
